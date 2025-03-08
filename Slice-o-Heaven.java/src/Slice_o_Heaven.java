bfb65fa68c04715fa26f464c6e87c442bc14dd9f
yizhi66 <2113517907@qq.com>
2025-03-08 20:50:14 +0800

    pizza work 5

--- Slice-o-Heaven.java/src/Slice_o_Heaven.java
+++ Slice-o-Heaven.java/src/Slice_o_Heaven.java
@@ -137,9 +137,20 @@ public class Slice_o_Heaven {
                 System.out.println("Too bad! You do not meet the conditions to get our 50% discount");
             }
         }
-        input.close();
+        if(diff.getYears() >= 120||diff.getYears() <= 5){
+            System.out.println("nvalid date. You are either too young or too dead to order. Please enter a valid date:");
+            
+        }
+        input.close(); 
+    }
+    public void makeCardPayment() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your card number:");
+        String cardNumber = scanner.nextLine(); 
+        System.out.println("Enter your expiration date (MM/YY):");
+        String expirationDate = scanner.nextLine();
 
-    
+
     }
     
 
@@ -147,13 +158,12 @@ public class Slice_o_Heaven {
 
     public void takeOrder(String id, String details, double ordertotal){
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Enter three ingredients for your pizza (use spaces to separate ingredients):");
         String[] ingredients = scanner.nextLine().split(" ");
         String ing1 = ingredients[0];
         String ing2 = ingredients[1];
         String ing3 = ingredients[2];
-
+              
     
         System.out.println("Enter size of pizza (Small, Medium, Large):");
         String pizzaSize = scanner.nextLine();
@@ -173,6 +183,7 @@ public class Slice_o_Heaven {
             isItYourBirthday();
             
         }
+    
 
     
 
@@ -202,6 +213,94 @@ public class Slice_o_Heaven {
 
 
     }
+
+    public void sizeChoice(String size){
+        System.out.println(" ”What size should your pizza be?\r\n" + //
+                        " 1. Large\r\n" + //
+                        " 2. Medium\r\n" + //
+                        " 3. Small\r\n" + //
+                        " Enter only one choice (1, 2, or 3):”");
+ 
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        switch(choice){
+            case 1:
+                size = "Large";
+                break;
+            case 2:
+                size = "Medium";
+                break;
+            case 3:
+                size = "Small";
+                break;
+            default:
+                q();
+                break;
+            }
+        System.out.println("”Do you want extra cheese (Y/N):”");
+        String extraCheese = scanner.nextLine();
+               
+    }
+    public void q(){
+        sizeChoice(DEF_ORDER_ID);
+    }
+  
+    public void sideDishChoice(String sideDish){
+        System.out.println("”Following are the side dish that go well with your pizza:\r\n" + //
+                        " 1. Calzone\r\n" + //
+                        " 2. Garlic bread\r\n" + //
+                        " 3. Chicken puff\r\n" + //
+                        " 4. Muffin\r\n" + //
+                        " 5. Nothing for me\r\n" + //
+                        " What would you like? Pick one (1, 2, 3,…):”");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                sideDish = "Calzone";   
+                break;    
+            case 2:
+                sideDish = "Garlic bread";
+                break; 
+            case 3:
+                sideDish = "Chicken puff";
+                break;   
+            case 4:
+                sideDish = "Muffin";
+                break;
+            case 5:
+                sideDish = "Nothing for me";
+                break;
+        }
+    }
+    public void drinksChoice(String drinks){
+        System.out.println(" ”Choose from one of the drinks below. We recommend Coca Cola:\r\n" + //
+                        " 1. Coca Cola\r\n" + //
+                        " 2. Cold coffee\r\n" + //
+                        " 3. Cocoa Drink\r\n" + //
+                        " 4. No drinks for me\r\n" + //
+                        " Enter your choice:”" );
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                drinks = "Coca Cola";
+                break; 
+            case 2:
+                drinks = "Cold coffee";
+                break;
+            case 3:
+                drinks = "Cocoa Drink";
+                break;
+            case 4:
+                drinks = "No drinks for me";
+                break;
+        }
+
+    }
     public void makepizza(String id, String details, double ordertotal){
         try{
             Thread.sleep(5000);
@@ -211,16 +310,18 @@ public class Slice_o_Heaven {
     
         System.out.println("Your pizza is ok!");
 
-        printReceipt();
+        toString();
     }
 
-    private void printReceipt(){
+    @Override
+    public String toString() {
         Order();
-        System.out.println("********RECEIPT********");
-
-        System.out.println("Order ID: " + orderID);
-        System.out.println("Order Details: " + orderDetails);
-        System.out.println("Order Total: " + orderTotal);
+        StringBuilder receipt = new StringBuilder();
+        receipt.append("********RECEIPT********\n");
+        receipt.append("Order ID: ").append(orderID).append("\n");
+        receipt.append("Order Details: ").append(orderDetails).append("\n");
+        receipt.append("Order Total: ").append(orderTotal);
+        return receipt.toString();
     }
     
 }
